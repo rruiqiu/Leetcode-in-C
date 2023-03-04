bd995ce15225e1f7e3b6b78b2dac107cefcb8cac
richardd <42120484+rruiqiu@users.noreply.github.com>
2023-03-04 16:08:58 -0500

    Time: 2 ms (49.36%), Space: 5.5 MB (25.89%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.c
+++ 0191-number-of-1-bits/0191-number-of-1-bits.c
@@ -1,7 +1,8 @@
 int hammingWeight(uint32_t n) {
     int count = 0;
+    //why we can't use n | 0, cuz it won't set the other bits to zero it will look like 0b000000000 which will give the origin value, not we want, so use & to get the first digit
     for(int i=0;i<32;i++){
-        //why we can't use n | 0, cuz it won't set the other bits to zero it will look like 0b000000000 which will give the origin value, not we want, so use & to get the first digit
+        
         if((n & 1) == 1){
             count++;
         }

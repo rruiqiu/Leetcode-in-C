c62674df13872899a3f09279dafb4d58c5685773
richardd <42120484+rruiqiu@users.noreply.github.com>
2023-03-06 18:42:14 -0500

    Time: 136 ms (89.90%), Space: 13.2 MB (21.56%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,22 @@
+int maxProfit(int* prices, int pricesSize){
+    int maxProfit = 0;
+    //[7,3,5,1,6,4]
+    
+    //start with index 1
+    //max = [1]-[0]
+    //first pointer move the right for each i and compare the max, if greater, update 
+    //second pointer track the smallest pointer before the fisrt pointer
+    int firstpointer = prices[0];
+    for(int i=1;i<pricesSize;i++){
+        if(prices[i] - firstpointer > maxProfit){
+            maxProfit = prices[i]- firstpointer;
+        }
+        if(firstpointer > prices[i]){
+            firstpointer = prices[i];            
+        }
+        
+    }
+    return maxProfit;
+    
+        
+}
